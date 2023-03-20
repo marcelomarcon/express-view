b3ed6d09ca5d0d657d6489753013e9bdd2f25568
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-03-20 14:00:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+const homeRouter = require("./routes/home");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("html", require("ejs").renderFile);
+
+// app.get("/home", (req, res) => {
+//   res.render("../views/home");
+// });
+
+// Routes
+app.use("/", homeRouter);
+
+
+// Running the server
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
