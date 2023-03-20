c18ad9e626f976272e95556f1e22b53d0da9fc71
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-03-20 16:14:45 -0400

    login form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -17,11 +18,13 @@ app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.urlencoded());
 
 // Routes
 app.use("/home", homeRouter);
 app.use("/login", loginRouter);
-
 
 // Running the server
 app.listen(process.env.PORT, () => {
--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,9 @@ router.get("/", (req, res) => {
   res.render(path.join(__dirname, "../", "views", "login.ejs"), {});
 });
 
+router.post("/validation", (req, res, next) => {
+    res.send('Hello World')
+  console.log(req.body);
+});
+
 module.exports = router;
