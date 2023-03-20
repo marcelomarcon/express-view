e8a088a6ddb6a6b0a1fe09f5a5bdaff2fc156da3
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-03-20 14:39:54 -0400

    refactored

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
-const app = express();
-dotenv.config();
 
+const app = express();
+
+// Routes imports
 const homeRouter = require("./routes/home");
 
+// Env variables
+dotenv.config();
+
+// Views engines and static files with SASS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
 app.engine("html", require("ejs").renderFile);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/home", homeRouter);
