abba998244f8f89045808e510cf38dc9f9aefe9d
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-03-21 09:39:22 -0400

    corrected notfound page

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(
   session({
     secret: process.env.SECRET_KEY,
@@ -30,18 +34,15 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Public Routes
 app.use("/", formRouter);
-app.use(handleNotFound);
 
 // Private Routes
 app.use(loginRequired);
-
 app.use("/home", homeRouter);
+
+// Errors Routes
+app.use(handleNotFound);
 
 // Running the server
 app.listen(process.env.PORT, () => {
