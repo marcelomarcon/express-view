2342aab1bccc964f1e0b38ede007f8dda7a0c0d3
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-03-21 09:09:25 -0400

    added messages for login page and register page

--- controllers/formController.js
+++ controllers/formController.js
@@ -3,7 +3,9 @@ const path = require("path");
 const User = require("../models/User");
 
 const getLoginPage = (req, res, next) => {
-  res.render(path.join(__dirname, "../", "views", "form.ejs"), {});
+  res.render(path.join(__dirname, "../", "views", "form.ejs"), {
+    message: "",
+  });
 };
 
 const postLoginPage = async (req, res, next) => {
@@ -12,12 +14,20 @@ const postLoginPage = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: lusername });
     if (!user) {
-      return res.status(400).send("Invalid username or password");
+      return res
+        .status(401)
+        .render(path.join(__dirname, "../", "views", "form.ejs"), {
+          message: "Invalid username or password!",
+        });
     }
 
     const isMatch = await user.comparePassword(lpassword);
     if (!isMatch) {
-      res.redirect("/login");
+      return res
+        .status(401)
+        .render(path.join(__dirname, "../", "views", "form.ejs"), {
+          message: "Invalid username or password!",
+        });
     }
 
     req.session.userId = user._id;
@@ -28,25 +38,33 @@ const postLoginPage = async (req, res, next) => {
 };
 
 const getRegisterPage = (req, res, next) => {
-    res.render(path.join(__dirname, "../", "views", "form.ejs"), {});
-}
+  res.render(path.join(__dirname, "../", "views", "form.ejs"), {});
+};
 
 const postRegisterPage = async (req, res, next) => {
   const { username, email, password, confirm_password } = req.body;
 
   // Check if the passwords match
   if (password !== confirm_password) {
-    return res.status(400).send("Passwords do not match");
+    return res
+      .status(400)
+      .render(path.join(__dirname, "../", "views", "form.ejs"), {
+        message: "Passwords don't match!",
+      });
   }
 
   try {
     const user = new User({ username, email, password });
     await user.save();
     res.redirect("/login");
-
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error registering new user");
+    // res.status(500).send("Error registering new user");
+    return res
+      .status(500)
+      .render(path.join(__dirname, "../", "views", "form.ejs"), {
+        message: "Error registering new user!",
+      });
   }
 };
 
